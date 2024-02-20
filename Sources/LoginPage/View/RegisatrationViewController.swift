7db94908d9e4272316619f368bf24f5fdd8e4d9e
YashChouhanWorkpulse <yash.chouhan@workpulse.com>
2024-02-20 17:56:45 +0530

    Added RegistrationViewModelProtocol init intop RegistrationViewController class

--- Sources/LoginPage/View/RegisatrationViewController.swift
+++ Sources/LoginPage/View/RegisatrationViewController.swift
@@ -9,21 +9,19 @@ import UIKit
 
 public class RegisatrationViewController: UIViewController {
 
+    private var viewModel: RegistrationViewModelProtocol
+    
+    public init(viewModel: RegistrationViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: "RegisatrationViewController", bundle: Bundle.module)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     public override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
